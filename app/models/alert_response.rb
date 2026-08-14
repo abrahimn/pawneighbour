2574f30b008f0866aecb20591b5432db45f38361
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-14 16:07:09 +1000

    alert_response has many photos

--- app/models/alert_response.rb
+++ app/models/alert_response.rb
@@ -1,6 +1,7 @@
 class AlertResponse < ApplicationRecord
   belongs_to :spotter, class_name: 'User'
   belongs_to :amber_alert
+  has_many_attached :photos
   validates :location, presence: true
   validates :date, presence: true
   validates :time, presence: true
