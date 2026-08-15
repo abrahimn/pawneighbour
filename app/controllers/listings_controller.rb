c5f1629e8db04765073111ee70b5874c4d126be8
Abrahim Nasreddine <abrahim.n036@gmail.com>
2026-08-15 14:19:04 +1000

    user can now add listing - do not accept edits to index.html - except to add post jobs button if possible

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,32 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
+
+  def new
+    @listing = Listing.new
+    @pets = current_user.pets.order(:name)
+  end
+
+  def create
+    @pet = current_user.pets.find(params[:listing][:pet_id])
+    @listing = @pet.listings.build(listing_params)
+
+    if @listing.save
+      redirect_to listings_path, notice: "Your job was posted."
+    else
+      @pets = current_user.pets.order(:name)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(
+      :listing_type,
+      :start_date,
+      :end_date,
+      :listing_note
+    )
+  end
 end
