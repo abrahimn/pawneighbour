16b559fb0cb759d66f904ef37e92ba880830f06b
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-18 21:27:33 +1000

    offer create method

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,4 +4,15 @@ class OffersController < ApplicationController
 
   def show
   end
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @offer = @listing.offers.new(user: current_user, status: "offered")
+
+    if @offer.save
+      redirect_to @listing, notice: "Offer sent to #{@listing.pet.user.name}"
+    else
+      redirect_to @listing, status: :unprocessable_entity
+    end
+  end
 end
