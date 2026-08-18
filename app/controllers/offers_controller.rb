8f9dbc9515b9fe7809a73aecf6f6139703c88c24
Abrahim Nasreddine <abrahim.n036@gmail.com>
2026-08-18 22:10:03 +1000

    owner can see offers on listings

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,13 @@
 class OffersController < ApplicationController
   def index
+    @listing = Listing.find(params[:listing_id])
+
+    unless @listing.pet.user == current_user
+      redirect_to listing_path(@listing), alert: "You are not authorized to view these offers."
+      return
+    end
+
+    @offers = @listing.offers
   end
 
   def show
