35dee1df709eb75029aec8e836e4a1d2910dc22b
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-20 15:31:07 +1000

    add long and lat for location to enable geocoder

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   has_one_attached :avatar
 
