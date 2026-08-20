cfd4436c63514001eca8dbfd23275f8103675bbf
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-20 12:36:11 +1000

    update the navbar design and listing show design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def avatar_for(user, size: 36)
+    if user.avatar.attached?
+      cl_image_tag(user.avatar.key,
+                   width: size * 2, height: size * 2, crop: :thumb, gravity: :face,
+                   class: "avatar", alt: user.name)
+    else
+      tag.span(user.name.to_s.first.upcase,
+               class: "avatar d-inline-flex align-items-center " \
+                      "justify-content-center bg-info text-white fw-semibold")
+    end
+  end
 end
