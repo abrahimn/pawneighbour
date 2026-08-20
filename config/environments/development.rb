47b618a84c0e5e63c338c4be9a568a2580ad499b
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-20 17:17:14 +1000

    avoid dup with pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,6 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
-  config.host.clear
-
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
