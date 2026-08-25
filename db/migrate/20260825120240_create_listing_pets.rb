f264ea804935a1272d9dfc648c0d81998463cce8
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-25 22:08:19 +1000

    add foreign key and index for listing_pets

--- db/migrate/20260825120240_create_listing_pets.rb
+++ db/migrate/20260825120240_create_listing_pets.rb
@@ -5,5 +5,7 @@ class CreateListingPets < ActiveRecord::Migration[8.1]
       t.references :pet,     null: false, foreign_key: true
       t.timestamps
     end
+
+    add_index :listing_pets, [:listing_id, :pet_id], unique: true
   end
 end
