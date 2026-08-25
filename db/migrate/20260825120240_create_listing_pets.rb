6747f7b2810d08346eda5f3fecf26a7a23284a9c
Luna Jia <xiaotong_jia@yahoo.com>
2026-08-25 22:17:08 +1000

    back fill the field to avoid error

--- db/migrate/20260825120240_create_listing_pets.rb
+++ db/migrate/20260825120240_create_listing_pets.rb
@@ -1,5 +1,5 @@
 class CreateListingPets < ActiveRecord::Migration[8.1]
-  def change
+  def up
     create_table :listing_pets do |t|
       t.references :listing, null: false, foreign_key: true
       t.references :pet,     null: false, foreign_key: true
@@ -7,5 +7,16 @@ class CreateListingPets < ActiveRecord::Migration[8.1]
     end
 
     add_index :listing_pets, [:listing_id, :pet_id], unique: true
+
+    execute <<~SQL
+      INSERT INTO listing_pets (listing_id, pet_id, created_at, updated_at)
+      SELECT id, pet_id, NOW(), NOW()
+      FROM listings
+      WHERE pet_id IS NOT NULL
+    SQL
+  end
+
+  def down
+    drop_table :listing_pets
   end
 end
--- db/migrate/20260825121302_remove_pet_from_listings.rb
+++ db/migrate/20260825121302_remove_pet_from_listings.rb
@@ -1,5 +1,18 @@
 class RemovePetFromListings < ActiveRecord::Migration[8.1]
-  def change
-    remove_reference :listings, :pet, null: false, foreign_key: true
+  def up
+    remove_reference :listings, :pet, foreign_key: true
+  end
+
+  def down
+    add_reference :listings, :pet, foreign_key: true
+
+    execute <<~SQL
+      UPDATE listings
+      SET pet_id = lp.pet_id
+      FROM listing_pets lp
+      WHERE lp.listing_id = listings.id
+    SQL
+
+    change_column_null :listings, :pet_id, false
   end
 end
